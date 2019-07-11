790253cd0b154b587dc9f9bf142cec956643a227
supervergil <vergil_007@139.com>
2019-07-11 17:38:09 +0800

    package

---
+++ build.js
@@ -0,0 +1,48 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+const fse = require("fs-extra");
+const archiver = require("archiver");
+
+const remoteDir = "./zblog_installer";
+
+const buildClient = dir => {
+  return new Promise((resolve, reject) => {
+    exec(`cd ${dir} & npm run build`, err => {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
+(async () => {
+  console.log("正在创建文件夹...");
+  fse.ensureDirSync(remoteDir);
+  fse.emptyDirSync(remoteDir);
+
+  // 构建admin和ucenter至www目录
+  console.log("正在构建客户端...");
+  await buildClient("interface");
+
+  // 提取对应的文件进入待上传文件夹
+  console.log("正在提取对应的文件进入待压缩文件夹...");
+  fse.copySync("./lib", `${remoteDir}/lib`);
+  fse.copySync("./package", `${remoteDir}/package`);
+  fse.copySync("./public", `${remoteDir}/public`);
+  fse.copySync("./index.js", `${remoteDir}/index.js`);
+  fse.copySync("./package-lock.json", `${remoteDir}/package-lock.json`);
+  fse.copySync("./package.json", `${remoteDir}/package.json`);
+
+  // 生成压缩包
+
+  const output = fs.createWriteStream("./zblog_installer.zip");
+  const archive = archiver("zip", {
+    zlib: { level: 9 }
+  });
+
+  archive.pipe(output);
+  archive.directory("./zblog_installer/", false);
+  archive.finalize();
+})();
